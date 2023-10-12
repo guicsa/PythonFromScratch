a7519486c53ef85ba26c58cdc0b12c905c936704
Guilherme.Amador <guilhermecesar28@gmail.com>
2023-10-12 14:24:33 +0200

    Teste
    Signed-off-by: Guilherme.Amador <guilhermecesar28@gmail.com>

---
+++ Calculos.py
@@ -0,0 +1,13 @@
+n1 = int(input('Digite o primeiro valor: '))
+ant = n1 - 1
+suc = n1 + 1
+dobro = n1 * 2
+triplo = n1 * 3
+raiz = n1 ** 0.5
+print(' o antecessor é {} e o sucessor é {}'.format(ant, suc))
+print(' o dobro é: {} \n o triplo é {} \n a raiz é : {}'.format(dobro, triplo, raiz))
+
+n2 = int(input('Digite um segundo número: '))
+media = (n1 + n2) / 2
+print('A média é {}'.format(media))
+print('ola2')
---
+++ main.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite o primeiro valor: '))
+ant = n1 - 1
+suc = n1 + 1
+dobro = n1 * 2
+triplo = n1 * 3
+raiz = n1 ** 0.5
+print(' o antecessor é {} e o sucessor é {}'.format(ant, suc))
+print(' o dobro é: {} \n o triplo é {} \n a raiz é : {}'.format(dobro, triplo, raiz))
