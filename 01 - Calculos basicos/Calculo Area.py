b6977bc3e22aea62f4251582d3bef8917a5ae134
Guilherme.Amador <guilhermecesar28@gmail.com>
2023-10-12 15:47:16 +0200

    First

--- 01 - Calculos basicos/Calculo Area.py
+++ 01 - Calculos basicos/Calculo Area.py
@@ -2,4 +2,5 @@ largura = int(input('Digite a largura: '))
 altura = int(input('Digite a altura: '))
 area = largura * altura
 qtdTinta = area/2
-print('A area corresponde a {} e a quantida de tinta necessária é {}'.format(area, qtdTinta))
+print('A area corresponde a {} e a quantidade de tinta necessária é {}'.format(area, qtdTinta))
+print('teste')
