1b90295b67a280574173cd7dd318c6aad4699dd5
Guilherme.Amador <guilhermecesar28@gmail.com>
2024-08-28 10:51:50 +0100

    First steps

---
+++ 00 - Python Hands-on/01 - Basics/01 - First.py
@@ -0,0 +1,2 @@
+greeting = "Hello Wolrd"
+print(greeting)
---
+++ 00 - Python Hands-on/01 - Basics/02 - Variables.py
@@ -0,0 +1,29 @@
+## CONCATENATION
+age = 30
+print("You are " + str(age) + " years old.")
+print("You are", age, "years old.")
+print(f"You are {age} years old")
+
+## INTEGER
+age = 21
+players = 2
+quantity = 5
+
+print(f"You are {age} years old.")
+print(f"There are {players} online")
+print(f"Would you like to buy {quantity} apples?")
+
+## FLOAT
+gpa = 3.2
+distance = 2.5
+price = 10.99
+
+print(f"Your gpa is {gpa}")
+print(f"You ran {distance }Km")
+print(f"The price is ${price}")
+
+
+## STRING
+
+
+## BOOLEAN
