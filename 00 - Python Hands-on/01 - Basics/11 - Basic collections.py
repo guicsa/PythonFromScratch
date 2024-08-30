86886a939ec980408f25fd22a9a972a2bd33f289
Guilherme.Amador <guilhermecesar28@gmail.com>
2024-08-30 18:40:26 +0100

    Basic Collection

--- 00 - Python Hands-on/01 - Basics/11 - Basic collections.py
+++ 00 - Python Hands-on/01 - Basics/11 - Basic collections.py
@@ -1,1 +1,36 @@
-list = ["apple","orange","banana","coconut"]
+# LIST
+fruits = ["apple","orange","banana","coconut"]
+
+print(fruits[::-1])
+print(fruits[0])
+
+for fruit in fruits:
+    print(fruit)
+
+#print(dir(fruits))
+#print(help(fruits))
+print(len(fruits))
+
+print("apple" in fruits)
+print("pineapple" in fruits)
+
+fruits[0] = "pineapple"
+
+print(fruits)
+
+fruits.append("grape")
+
+print(fruits)
+
+fruits.remove("pineapple")
+fruits.insert(0,"pear")
+
+print(fruits)
+
+fruits.sort()
+
+print(fruits)
+
+fruits.reverse()
+
+print(fruits)
