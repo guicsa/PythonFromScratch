e852ef3242b62022a6557f31b2f1cd9659b7b1f6
Roberta Campelo <roberta.campelo8@gmail.com>
2024-08-31 09:44:36 +0100

    Basic collections

--- 00 - Python Hands-on/01 - Basics/11 - Basic collections.py
+++ 00 - Python Hands-on/01 - Basics/11 - Basic collections.py
@@ -1,4 +1,5 @@
 # LIST
+## Ordered and changeable. Duplicates are OK
 fruits = ["apple","orange","banana","coconut"]
 
 print(fruits[::-1])
@@ -34,3 +35,37 @@ print(fruits)
 fruits.reverse()
 
 print(fruits)
+print(fruits.index("orange"))
+
+fruits.clear()
+
+print(fruits)
+
+# SET
+# {} Unordered and immutable, but Add/Remove OK. No duplicates.
+print("\n")
+print("\n")
+print("\n")
+fruits = {"apple","orange","banana","coconut"}
+print(fruits)
+fruits.add("pineapple")
+print(fruits)
+fruits.remove("apple")
+print(fruits)
+fruits.pop()
+print(fruits)
+fruits.clear()
+print(fruits)
+
+# Tuple
+# () Ordered and unchangeable. Duplicates OK. FASTER
+print("\n")
+print("\n")
+print("\n")
+fruits = ("apple","orange","banana","coconut")
+print(fruits)
+fruits = ("apple","orange","banana","coconut","coconut")
+print(fruits)
+print("coconut" in fruits)
+print(fruits.index("orange"))
+print(fruits.count("coconut"))
